e72aaf21a76b2280614c7edae29d8fbc9d6d02f2
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-10 18:58:19 +0300

    Update HT_2(2).py

--- HT_2/HT_2(2).py
+++ HT_2/HT_2(2).py
@@ -7,6 +7,5 @@ second_input_colors = input('Enter the second list of colors: \n')
 color_list_1 = set(first_input_colors.split())
 color_list_2 = set(second_input_colors.split())
 
-
 print("\nColors that are not in the second list:")
 print(color_list_1.difference(color_list_2))
