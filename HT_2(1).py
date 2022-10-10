10a4a65b58a55802c5cd6fe8de8948eb4b223edb
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-10 18:33:25 +0300

    Delete HT_2(1).py

--- HT_2(1).py
+++
@@ -1,9 +0,0 @@
-enter_numbers = input('enter numbers: \n')
-
-list_numbers = [enter_numbers]
-print('List numbers: ')
-print(list_numbers)
-
-tuple_numbers = tuple(list_numbers)
-print('Tuple numbers: ')
-print(tuple_numbers)
