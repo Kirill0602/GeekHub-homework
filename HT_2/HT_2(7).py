e66bbd352feb353dc3e77922f8429d9a32a3403c
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-10 19:01:29 +0300

    Update HT_2(7).py

--- HT_2/HT_2(7).py
+++ HT_2/HT_2(7).py
@@ -1,3 +1,6 @@
+#7. Write a script to concatenate all elements in a list into a string and print it. 
+#List must include both strings and integers and must be hardcoded.
+
 input_value = input('Enter value: \n')
 b = ''
 for i in input_value:
