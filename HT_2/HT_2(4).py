5c3dddc790b7847d1188bcc7e077992f5da8a6a3
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-10 18:59:28 +0300

    Update HT_2(4).py

--- HT_2/HT_2(4).py
+++ HT_2/HT_2(4).py
@@ -1,3 +1,6 @@
+#4. Write a script which accepts a <number> from user and then <number> times asks user for string input. 
+#At the end script must print out result of concatenating all <number> strings.
+
 number = int(input('Enter number: \n'))
 for i in set(range(number + 1)):
     z = str(number)
