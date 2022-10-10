da9a54daca0474bcfbee4e3279d96c305a69ed98
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-10 18:58:50 +0300

    Update HT_2(3).py

--- HT_2/HT_2(3).py
+++ HT_2/HT_2(3).py
@@ -1,3 +1,5 @@
+#3. Write a script which accepts a <number> from user and print out a sum of the first <number> positive integers.
+
 number = int(input("Enter number: \n"))
 result = sum(range(number + 1))
 print("Sum of the first", number ,"positive integers:",result)
