7226a03f7dc073181f9ad50cf93d6770f8db4fc3
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-10 19:00:01 +0300

    Update HT_2(5).py

--- HT_2/HT_2(5).py
+++ HT_2/HT_2(5).py
@@ -1,2 +1,4 @@
+#5. Write a script which accepts a decimal number from user and converts it to hexadecimal.
+
 number = int(input('Enter your number: \n'))
 print('Your number converts to hexadecimal:', hex(number))
