7849d596048ef44f39e3ef76d2546ef9e00b9971
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-10 22:19:27 +0300

    Update HT_2(2).py

--- HT_2/HT_2(2).py
+++ HT_2/HT_2(2).py
@@ -1,11 +1,9 @@
 #2. Write a script which accepts two sequences of comma-separated colors from user. 
 #Then print out a set containing all the colors from color_list_1 which are not present in color_list_2.
 
-first_input_colors = input('Enter the first list of colors: \n')
-second_input_colors = input('Enter the second list of colors: \n')
-
-color_list_1 = set(first_input_colors.split())
-color_list_2 = set(second_input_colors.split())
-
-print("\nColors that are not in the second list:")
+color_list_1 = input()
+color_list_2 = input()
+color_list_1 = set(color_list_1.split(sep=', '))
+color_list_2 = set(color_list_2.split(sep=', '))
+print("\nDifferenct of color_list_1 and color_list_2:")
 print(color_list_1.difference(color_list_2))
