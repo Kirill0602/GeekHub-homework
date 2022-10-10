ca83e093ad1a0238f5aa9e03dff1ed5f8c9c2f55
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-10 20:43:10 +0300

    Update HT_2(1).py

--- HT_2/HT_2(1).py
+++ HT_2/HT_2(1).py
@@ -1,11 +1,7 @@
 #1. Write a script which accepts a sequence of comma-separated numbers from user and generates a list and a tuple with those numbers.
 
 enter_numbers = input('enter numbers: \n')
-
-list_numbers = [enter_numbers]
-print('List numbers: ')
-print(list_numbers)
-
-tuple_numbers = tuple(list_numbers)
-print('Tuple numbers: ')
-print(tuple_numbers)
+list = enter_numbers.split(',')
+tuple = tuple(list)
+print('List numbers: ', list)
+print('Tuple numbers: ', tuple)
