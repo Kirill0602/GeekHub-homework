608ea67b8724cf84ec3b75c7d96e25f329d1a22e
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-10 23:46:21 +0300

    Update HT_2(7).py

--- HT_2/HT_2(7).py
+++ HT_2/HT_2(7).py
@@ -1,10 +1,5 @@
 #7. Write a script to concatenate all elements in a list into a string and print it. 
 #List must include both strings and integers and must be hardcoded.
 
-input_value = input('Enter value: \n')
-b = ''
-for i in input_value:
-    if i == ',' or i == ' ':
-        i = ''
-    b += i
-print(b)
+list1 = [1, 5, 'g', 5, 'fdf']
+print("".join(map(str, list1)))
