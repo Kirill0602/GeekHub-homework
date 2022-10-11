a2924e65b582ee346fbe5fd4584c2683d543bcdb
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-11 11:21:03 +0300

    Update HT_2(4).py

--- HT_2/HT_2(4).py
+++ HT_2/HT_2(4).py
@@ -1,9 +1,9 @@
 #4. Write a script which accepts a <number> from user and then <number> times asks user for string input. 
 #At the end script must print out result of concatenating all <number> strings.
 
-number = int(input('Enter number: \n'))
-for i in set(range(number + 1)):
-    z = str(number)
-    
-print('Result of concatenating all: ', (z*i))    
-
+result = []
+cnt = int(input("Insert count:"))
+for i in range(cnt):
+    result.append(input())
+print("Result:")
+print(" ".join(result))
