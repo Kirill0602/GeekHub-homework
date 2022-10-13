1153183eb6fd43378159239fe0598180b682497a
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-13 23:33:48 +0300

    Update HT_3(4).py

--- HT_3/HT_3(4).py
+++ HT_3/HT_3(4).py
@@ -10,14 +10,8 @@ print('Third dictionary: ', {'AUD': 19.2, 'name': 'Tom'})
 dict_1 = {'foo': 'bar', 'bar': 'buz'}
 dict_2 = {'dou': 'jones', 'USD': 36}
 dict_3 = {'AUD': 19.2, 'name': 'Tom'}
-dict_4 = {}
+
 
 change_dict = input('Change first dictionary: ')
 dict_1['bar'] = change_dict
-print('Changed first dictionary: ', dict_1)
-
-for d in (dict_1, dict_2, dict_3):
-    dict_4.update(d)
-    
-print('Concatenate dictionary: \n', dict_4)
-    
+print('Changed first dictionary: \n', dict_1,'\n', dict_2,'\n', dict_3)
