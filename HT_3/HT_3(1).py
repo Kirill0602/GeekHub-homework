54c9465fbb3db1ce1cb2f72d72e96b8f2280ffa5
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-13 21:23:22 +0300

    Update HT_3(1).py

--- HT_3/HT_3(1).py
+++ HT_3/HT_3(1).py
@@ -1,3 +1,9 @@
+'''
+1. Write a script that will run through a list of tuples and replace the last value for each tuple. 
+The list of tuples can be hardcoded. The "replacement" value is entered by user.
+The number of elements in the tuples must be different.
+'''
+
 print('Your list of tuples: ', [(1, 2, 3, 5), (3, 2, 4), (5, 6)])
 
 list_of_tuples = list([(1, 2, 3, 5), (3, 2, 4), (5, 6)])
