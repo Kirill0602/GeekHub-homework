3fa496758bc16ba9bb278802dde432d3d21cea51
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-14 10:03:37 +0300

    Update HT_3(4).py

--- HT_3/HT_3(4).py
+++ HT_3/HT_3(4).py
@@ -10,8 +10,10 @@ print('Third dictionary: ', {'AUD': 19.2, 'name': 'Tom'})
 dict_1 = {'foo': 'bar', 'bar': 'buz'}
 dict_2 = {'dou': 'jones', 'USD': 36}
 dict_3 = {'AUD': 19.2, 'name': 'Tom'}
-
-
 change_dict = input('Change first dictionary: ')
 dict_1['bar'] = change_dict
-print('Changed first dictionary: \n', dict_1,'\n', dict_2,'\n', dict_3)
+
+dict_1.update(dict_2)
+dict_1.update(dict_3)
+print(dict_1)
+
