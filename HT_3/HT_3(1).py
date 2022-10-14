29de53c487aecb211fa2312958e223ab5a93e13b
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-14 12:46:11 +0300

    Update HT_3(1).py

--- HT_3/HT_3(1).py
+++ HT_3/HT_3(1).py
@@ -4,21 +4,20 @@ The list of tuples can be hardcoded. The "replacement" value is entered by user.
 The number of elements in the tuples must be different.
 '''
 
-print('Your list of tuples: ', [(1, 2, 3, 5), (3, 2, 4), (5, 6)])
 
-list_of_tuples = list([(1, 2, 3, 5), (3, 2, 4), (5, 6)])
+list_of_tuple = [(1, 2, 3), ('abc', 'qwert'), ('y',), ()]
+print('List of tuple: ', list_of_tuple)
 
-for i in range(len(list_of_tuples)):
-    input1 = int(input('Enter first number and press Enter: '))
-    tuple1_in_list = list(list_of_tuples[0])
-    tuple1_in_list[3] = input1
-    input2 = int(input('Enter second number and press Enter: '))
-    tuple2_in_list = list(list_of_tuples[1])
-    tuple2_in_list [2] = input2
-    input3 = int(input('Enter third number and press Enter: '))
-    tuple3_in_list = list(list_of_tuples[2])
-    tuple3_in_list[1] = input3
-    print('Сhanged the last values ​​of each tuple: \n',
-          [tuple(tuple1_in_list),tuple(tuple2_in_list),tuple(tuple3_in_list)])
-    if True==True:
-        break
+
+change_value = input('Enter value: ')
+
+data_storage = []
+for i in list_of_tuple:
+    li = list(i)
+    li = li[:-1]
+    li.append(change_value)
+    li = tuple(li)
+    data_storage += [li]
+print('Change_value:',data_storage)
+
+
