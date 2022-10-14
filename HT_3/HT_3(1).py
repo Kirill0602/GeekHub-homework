a66c3e44da6774a720adda9825519fd28aeb7745
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-14 17:01:23 +0300

    Update HT_3(1).py

--- HT_3/HT_3(1).py
+++ HT_3/HT_3(1).py
@@ -4,7 +4,6 @@ The list of tuples can be hardcoded. The "replacement" value is entered by user.
 The number of elements in the tuples must be different.
 '''
 
-
 list_of_tuple = [(1, 2, 3), ('abc', 'qwert'), ('y',), ()]
 print('List of tuple: ', list_of_tuple)
 
@@ -13,11 +12,14 @@ change_value = input('Enter value: ')
 
 data_storage = []
 for i in list_of_tuple:
-    li = list(i)
-    li = li[:-1]
-    li.append(change_value)
-    li = tuple(li)
-    data_storage += [li]
+    if i:
+        li = list(i)
+        li = li[:-1]
+        li.append(change_value)
+        li = tuple(li)
+        data_storage += [li]
+    else:
+        data_storage.append(i)
 print('Change_value:',data_storage)
 
 
