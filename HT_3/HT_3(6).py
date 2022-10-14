0bec57c8eb7ae8283e764e61ecbc8514874bc7c1
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-14 17:06:12 +0300

    Update HT_3(6).py

--- HT_3/HT_3(6).py
+++ HT_3/HT_3(6).py
@@ -3,16 +3,22 @@
 '''
 
 students = {
-  'Anatoly': 25,
-  'Kyrylo': 30,
+  'Anatoly': 25.6,
+  'Kyrylo': '30',
   'Gregory': 20,
-  'Sergey': 29, 
+  'Sergey': 29.35, 
 }
 
 print('Dictionary:', students)
 p = input('Do you want to know the maximum and minimum values of dictionaries?\nPress Enter \n')
+
+my_list = []
+for i in students.values():
+    if type(i) == int or type(i) == float:
+        my_list.append(i)
+        
 if p == '':
-    print('Maximum value in a dictionary:',max(students.values()))
-    print('Minimum value in a dictionary:',min(students.values()))
+    print('Maximum value in a dictionary:',max(my_list))
+    print('Minimum value in a dictionary:',min(my_list))
 else:
     print('Goodbye')
