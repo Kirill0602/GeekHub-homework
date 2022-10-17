babff2d387339796526b66d967b97a8c0eaab89d
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-17 13:11:21 +0300

    Update HT_4(5).py

--- HT_4/HT_4(5).py
+++ HT_4/HT_4(5).py
@@ -1,34 +1,32 @@
 def calc(x, action, y):
     while True:
         if action == '+':
-            print('%.2f + %.2f = %.2f' % (x, y, x+y))
+            print('{} + {} = {}'.format(x, y, x+y))
         elif action == '-':
-            print('%.2f - %.2f = %.2f' % (x, y, x-y))
-        #elif action == '**':
-            #print('%.2f ** %.2f = %.2f' % (x, y, x**y))
+            print('{} - {} = {}'.format(x, y, x-y))
+        elif action == '**':
+            print('{} ** {} = {}'.format(x, y, x**y))
         elif action == '*':
-            print('%.2f * %.2f = %.2f' % (x, y, x*y))
-        #elif action == '//':
-            #print('%.2f // %.2f = %.2f' % (x, y, x//y))
+            print('{} * {} = {}'.format(x, y, x*y))
+        elif action == '//':
+            print('{} // {} = {}'.format(x, y, x//y))
+        elif action == '%':
+            print('{} % {} = {}'.format(x, y, x%y))
         elif action == '/':
             if y != 0:
-                print('%.2f / %.2f = %.2f' % (x, y, x/y))
+                print('{} / {} = {}'.format(x, y, x/y))
             else: 
                 print("Division by zero!")
 
         break
         
 try:
+    
     x = float(input("Enter the first number: "))
-    action = input("Action +, -, *, /: ")
+    action = input("Action +, -, *, **, /, //, % : ")
     y = float(input("Enter the second number: "))
+    calc(x, action, y)
 except ValueError:
     print("It's not a number")
-try:
-    calc(x, action, y)
-except NameError:
-    print('End program')
-    
-
 
 
