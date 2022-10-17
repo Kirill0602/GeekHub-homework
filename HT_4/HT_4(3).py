516a445b34188d0849d1d6bb210bff8528a6099c
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-17 12:47:32 +0300

    Update HT_4(3).py

--- HT_4/HT_4(3).py
+++ HT_4/HT_4(3).py
@@ -1,3 +1,12 @@
+'''
+3. Користувач вводить змінні "x" та "y" з довільними цифровими значеннями. 
+Створіть просту умовну конструкцію (звiсно вона повинна бути в тiлi ф-цiї), 
+під час виконання якої буде перевірятися рівність змінних "x" та "y" та у випадку нерівності - виводити ще і різницю.
+    Повинні працювати такі умови (x, y, z заміність на відповідні числа):
+    x > y;       вiдповiдь - "х бiльше нiж у на z"
+    x < y;       вiдповiдь - "у бiльше нiж х на z"
+    x == y.      відповідь - "х дорівнює y"
+'''
 x = input('Input first number: ')
 y = input('Input second number: ')
 
@@ -11,12 +20,12 @@ def name(x, y):
 
     if x > y:
         result = x - y
-        return '%s > %s on ' %(x,y) + str(result) 
+        return '{} > {} on '.format(x,y) + str(result) 
     elif x < y:
         result = y - x
-        return '%s < %s on ' %(x,y)+str(result)
+        return '{} < {} on '.format(x,y)+str(result)
     elif x == y:
-        return '%s = %s ' %(x,y)    
+        return '{} = {} '.format(x,y)    
 
 
 
