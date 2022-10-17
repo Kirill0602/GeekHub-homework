e155aede65f5d8b154b718233011b8bbd71e2b7d
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-17 21:48:51 +0300

    Update HT_4(2).py

--- HT_4/HT_4(2).py
+++ HT_4/HT_4(2).py
@@ -5,29 +5,33 @@
 обробляє їх результат та також повертає результат своєї роботи. 
 Таким чином ми будемо викликати одну (четверту) функцiю, а вона в своєму тiлi - ще 3.
 '''
-def launch_all():
-    print('I am fourth function.')
+def func(enter):
+    print(f'Output of the first function: {enter}')
+    return enter
 
-    def func(enter):
-        print('Output of the first function: {}'.format(enter))
+def sum_func(a, b):
+    result = a + b
+    result1 = print(f'Output of the first function: {result}')
+    return result1
 
 
-    def sum_func(a, b):
-        print('Output of the second function(sum of numbers): {}'.format(a + b))
-
-
-    def multiplication_func(q, w):
-        print('Output of the third function(multiplication of numbers 9*9): {}'.format(q*w))
+def multiplication_func(q, w):
+    print(f'Output of the third function(multiplication of numbers 9*9): {q * w}')
+    return q * w
 
         
-
+def launch_all():
+    print('I am fourth function.')
     func(enter)
     sum_func(a, b)
     multiplication_func(9, 9)
+    return 
     
 enter = input('Write something: ')
 a = int(input('input first number: '))
 b = int(input('Input second number: '))
 
 
+
 launch_all()
+
