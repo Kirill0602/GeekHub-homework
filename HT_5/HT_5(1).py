b936b2dc505a564590aec3dee86007798e06ee3a
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-19 11:58:27 +0300

    Add files via upload

---
+++ HT_5/HT_5(1).py
@@ -0,0 +1,14 @@
+def square():
+
+    side_of_square = float(input('Input side of square: '))
+    perimeter_square = side_of_square * 4
+    area_of_square = side_of_square * side_of_square
+    diagonal_square = side_of_square * (2 ** 0.5)
+    result = (perimeter_square, area_of_square, diagonal_square)
+    print(f'Perimetr: {perimeter_square} \nArea: {area_of_square} \nDiagonal: {diagonal_square}')
+    return result
+
+square()
+
+
+
