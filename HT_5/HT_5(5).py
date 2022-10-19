1375ee995826fa0df2940cef3b932b81ce33a4f9
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-19 16:23:42 +0300

    Add files via upload

---
+++ HT_5/HT_5(5).py
@@ -0,0 +1,16 @@
+'''
+5. Написати функцію <fibonacci>, яка приймає один аргумент
+і виводить всі числа Фібоначчі, що не перевищують його.
+
+'''
+def fibonacci(number):
+    b = 1
+    c = 1
+    while b <= number:
+        print(b, end=' ')
+        (b, c) = (c, b + c)
+    return b-c
+
+number = int(input('Input number: '))
+print('Fibonacci numbers that do not exceed the entered number:')    
+fibonacci(number)
