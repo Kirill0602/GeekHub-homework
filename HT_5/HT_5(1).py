769d81bd6c1eb3b34628493a6147eafba2c20dc2
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-21 09:59:49 +0300

    Update HT_5(1).py

--- HT_5/HT_5(1).py
+++ HT_5/HT_5(1).py
@@ -2,17 +2,16 @@
 1. Написати функцію <square>, яка прийматиме один аргумент - сторону квадрата, 
 і вертатиме 3 значення у вигляді кортежа: периметр квадрата, площа квадрата та його діагональ.
 '''
-def square():
+def square(side_of_square):
 
-    side_of_square = float(input('Input side of square: '))
     perimeter_square = side_of_square * 4
     area_of_square = side_of_square * side_of_square
     diagonal_square = side_of_square * (2 ** 0.5)
     result = (perimeter_square, area_of_square, diagonal_square)
     print(f'Perimetr: {perimeter_square} \nArea: {area_of_square} \nDiagonal: {diagonal_square}')
     return result
-
-square()
+side_of_square = float(input('Input side of square: '))
+square(side_of_square)
 
 
 
