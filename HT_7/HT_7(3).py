86ec4bbd68195438da00f40578ce56a64233e268
Kyrylo Kuzmenko <93709002+KyryloKuzmenko@users.noreply.github.com>
2022-10-27 19:30:36 +0300

    Add files via upload

---
+++ HT_7/HT_7(3).py
@@ -0,0 +1,35 @@
+'''
+3. Всі ви знаєте таку функцію як <range>. Напишіть свою реалізацію цієї функції.
+Тобто щоб її можна було використати у вигляді:
+    for i in my_range(1, 10, 2):
+        print(i)
+    1
+    3
+    5
+    7
+    9
+   P.S. Повинен вертатись генератор.
+   P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній:
+   https://docs.python.org/3/library/stdtypes.html#range
+   P.P.P.S Не забудьте обробляти невалідні ситуації (типу range(1, -10, 5) тощо).
+   Подивіться як веде себе стандартний range в таких випадках.
+'''
+
+
+def my_range(start, stop, step=2):
+
+    
+    start -= step
+    while True:
+        if start > stop and step < 0:
+            if start + step <= stop:
+                break
+        else:
+            if start + step >= stop:
+                break
+        start += step
+        yield round(start, 2)
+
+
+for i in my_range(1, 10, 2):
+    print(i)
